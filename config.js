68fd75a34ba7db4f1a7f9f2c6ed8c2e3e5ffdbd2
Kareem <kemo.friends@gmail.com>
2026-05-08 19:56:28 +0300

    Connect Formspree save endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Optional public save endpoint for the hosted form.
 // Leave blank to use local_server.py locally or to download a JSON fallback on GitHub Pages.
-window.VALOR_SAVE_ENDPOINT = '';
+window.VALOR_SAVE_ENDPOINT = 'https://formspree.io/f/xzdojdpe';
